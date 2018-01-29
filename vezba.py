ff668625ced46f1bf825d9c156ca5766a991e41b
Milosh Aritonski <maritonski@gmail.com>
2018-01-29 22:37:48 +0100

    Some scraping tuts ..

--- vezba.py
+++ vezba.py
@@ -3,19 +3,27 @@ from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
 
-quote_page = 'https://www.bloomberg.com/quote/SPX:IND'
-page = urllib.request.urlopen(quote_page)
+quote_page = ['https://www.bloomberg.com/quote/SPX:IND','http://www.bloomberg.com/quote/CCMP:IND']
 
-soup = BeautifulSoup(page, 'html.parser')
-name_box = soup.find('h1', attrs={'class':'name'})
+data=[]
 
-name=name_box.text.strip() # strip is used to remove starting and trailing
-print(name)
+for pg in quote_page:
+    page=urllib.request.urlopen(pg)
+    soup=BeautifulSoup(page, 'html.parser')
+    name_box = soup.find('h1', attrs={'class':'name'})
+    name=name_box.text.strip() # strip is used to remove starting and trailing
+    print(name)
+
+    price_box = soup.find('div', attrs={'class':'price'})
+    price=price_box.text
+    print(price)
+    data.append((name,price))
 
 # open a csv file with append, so old data will be erased
 with open ('index.csv', 'a') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow([name, datetime.now()])
+    for name, price in data:
+        writer.writerow([name, price, datetime.now()])
 
 
 # https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
