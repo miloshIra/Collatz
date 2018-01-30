3aec34297fc6ca3e776f9bb69872a322e356de9b
Milosh Aritonski <maritonski@gmail.com>
2018-01-30 23:40:14 +0100

    Epic hack-n-slash code, bolji mesarsko da ucev

---
+++ WebScrape/Emily/emilyscrapper.py
@@ -0,0 +1,32 @@
+import requests, os, bs4
+
+url = ''   # starting URL
+os.makedirs('Emily Heaven', exist_ok=True)                       # stores in Emily heaven
+
+while not url.endswith('#'):
+
+    print('Downloading image %s...' %url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    element = soup.select('#primary img')
+
+    if element == []:
+        # photo=image.get('data-lazy') # OVA RABOTI ZIMI SE
+
+        print('Could not find image')
+
+    else:
+        elementUrl = element[0].get('src')
+        res = requests.get(elementUrl)
+        res.raise_for_status()
+
+        imageFile = open(os.path.join('Emily Heaven', os.path.basename(elementUrl)), 'wb')
+        for chunk in res.iter_content():
+            imageFile.write(chunk)
+        imageFile.close()
+
+
+
+print('Done')
