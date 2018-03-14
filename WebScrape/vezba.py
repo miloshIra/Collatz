a6ed95897b02ec8a1b3490393b4fba7a4a54b9c9
Milosh Aritonski <30418161+miloshIra@users.noreply.github.com>
2018-03-14 14:13:49 +0100

    Add files via upload
    Added panda dataframe and saved it in excel file...

--- WebScrape/vezba.py
+++ WebScrape/vezba.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
+from pandas import ExcelWriter
+import datetime
+
 
 
 request = requests.get('https://www.bookdepository.com/search?searchTerm=learning+python&search=Find+book')
@@ -14,11 +18,9 @@ price = soup.find_all('p', attrs={'class':'price'})
 for title in title:
     name=title.text.strip()
     list1.append(name)
-    print(list1)
 
 for price in price:
     string_price=(price.text.strip())
-    print(string_price)
 
     if string_price[1] != ",":
         price_whole_number = string_price[0:2]
@@ -29,14 +31,28 @@ for price in price:
     price_whole = float(price_whole_number + "." + price_decimal)
     list2.append(price_whole)
 
-
 k = list(zip(list1,list2))
 
-for i in k:
-        with open ('books.csv', 'w') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow([k])
+df=pd.DataFrame(k, columns=['Title', 'Price'])
+print(df)
+
+date = datetime.datetime.now()
+print(date)
+
+writer = ExcelWriter('Books.xlsx')
+df.to_excel(writer, 'Denes')
+writer.save()
 
 
+# for i in k:
+#         with open ('books.csv', 'w') as csv_file:
+#             writer = csv.writer(csv_file)
+#             writer.writerow([k])
+
+# df = pd.read_csv('books.csv')
+# print(df)
+# df.to_csv('data.csv')
+#
+#
 
 # https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
