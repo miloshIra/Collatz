cff229ccc7f6eecdbabd531347787b2ebb3fcd86
Milosh Aritonski <30418161+miloshIra@users.noreply.github.com>
2018-03-20 15:43:30 +0100

    Add files via upload
    Don't know if this BookCRW can work like this.. check later.

--- WebScrape/vezba.py
+++ WebScrape/vezba.py
@@ -4,45 +4,55 @@ import csv
 import pandas as pd
 from pandas import ExcelWriter
 import datetime
+from flask import Flask, render_template
 
+app=Flask(__name__)
 
+@app.route('/')
+def home_template():
+    render_template('index.html')
 
-request = requests.get('https://www.bookdepository.com/search?searchTerm=learning+python&search=Find+book')
-content = request.content
-list1 = []
-list2 = []
-soup = BeautifulSoup(content, 'html.parser')
-title = soup.find_all('h3', attrs={'class':'title'})
-price = soup.find_all('p', attrs={'class':'price'})
+def crawl():
+    request = requests.get('https://www.bookdepository.com/search?searchTerm=learning+python&search=Find+book')
+    content = request.content
+    list1 = []
+    list2 = []
+    soup = BeautifulSoup(content, 'html.parser')
+    title = soup.find_all('h3', attrs={'class':'title'})
+    price = soup.find_all('p', attrs={'class':'price'})
 
-for title in title:
-    name=title.text.strip()
-    list1.append(name)
+    for title in title:
+        name=title.text.strip()
+        list1.append(name)
 
-for price in price:
-    string_price=(price.text.strip())
+        for price in price:
+            string_price=(price.text.strip())
 
-    if string_price[1] != ",":
-        price_whole_number = string_price[0:2]
-        price_decimal = string_price[3:5]
-    else:
-        price_whole_number = string_price[0:1]
-        price_decimal = string_price[3:5]
-    price_whole = float(price_whole_number + "." + price_decimal)
-    list2.append(price_whole)
+            if string_price[1] != ",":
+                price_whole_number = string_price[0:2]
+                price_decimal = string_price[3:5]
+            else:
+                price_whole_number = string_price[0:1]
+                price_decimal = string_price[3:5]
+                price_whole = float(price_whole_number + "." + price_decimal)
+                list2.append(price_whole)
 
-k = list(zip(list1,list2))
+            k = list(zip(list1,list2))
 
-df=pd.DataFrame(k, columns=['Title', 'Price'])
-print(df)
+            df=pd.DataFrame(k, columns=['Title', 'Price'])
+            print(df)
 
 date = datetime.datetime.now()
 print(date)
 
-writer = ExcelWriter('Books.xlsx')
-df.to_excel(writer, 'Denes')
-writer.save()
+def write_to_excel():
+    writer = ExcelWriter('Books.xlsx')
+    df.to_excel(writer, 'Denes')
+    writer.save()
 
+
+if  __name__=='__main__':
+    app.run(port=5000)
 
 # for i in k:
 #         with open ('books.csv', 'w') as csv_file:
